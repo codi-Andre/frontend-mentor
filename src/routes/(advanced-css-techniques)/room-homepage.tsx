d6c586339dd87f0badb2907b35b9800f61b06167
André Miranda <aegis.andre@gmail.com>
2025-04-20 20:57:35 -0300

    :sparkles: feat: finish room homepage

--- src/routes/(advanced-css-techniques)/room-homepage.tsx
+++ src/routes/(advanced-css-techniques)/room-homepage.tsx
@@ -197,16 +197,18 @@ function RouteComponent() {
             alt="dark living room with furniture"
           />
 
-          <h2 className={styles["about__title"]}>About our furniture</h2>
+          <div className={styles["about__content"]}>
+            <h2 className={styles["about__title"]}>About our furniture</h2>
 
-          <p className={styles["about__paragraph"]}>
-            Our multifunctional collection blends design and function to suit
-            your individual taste. Make each room unique, or pick a cohesive
-            theme that best express your interests and what inspires you. Find
-            the furniture pieces you need, from traditional to contemporary
-            styles or anything in between. Product specialists are available to
-            help you create your dream space.
-          </p>
+            <p className={styles["about__paragraph"]}>
+              Our multifunctional collection blends design and function to suit
+              your individual taste. Make each room unique, or pick a cohesive
+              theme that best express your interests and what inspires you. Find
+              the furniture pieces you need, from traditional to contemporary
+              styles or anything in between. Product specialists are available
+              to help you create your dream space.
+            </p>
+          </div>
 
           <img
             className={styles["about__light-image"]}
